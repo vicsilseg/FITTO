1c178cb6672c9f52f3bfc2457c3bf1a0ef58a62c
Victor Silva Segarra <vicsilseg@gmail.com>
2018-06-03 23:01:33 +0200

    mapState in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import post from './components/Post.vue'
 /* import { mapState } from "vuex" */
 export default {
@@ -11,12 +12,17 @@ export default {
   components: {
     post
   },
-  data () {
-    return {}
-  },
   beforeMount () {
     this.$router.push('/login')
-  }
+  },
+  computed: mapState({
+    nombre: state => state.usuario.nombre,
+    apellidos: state => state.usuario.apellidos,
+    bio: state => state.usuario.bio,
+    email: state => state.usuario.email,
+    codigo: state => state.usuario.codigo,
+    avatar: state => state.usuario.avatar
+  })
 }
 </script>
 
